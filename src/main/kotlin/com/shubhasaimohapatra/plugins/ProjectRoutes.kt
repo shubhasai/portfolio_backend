255c97e4d95ddb36f0ca1d0a4409ab7e12f443c1
shubhasai <mshubhasai2002@gmail.com>
2023-12-31 00:43:00 +0530

    Project Route Optimized

--- src/main/kotlin/com/shubhasaimohapatra/plugins/ProjectRoutes.kt
+++ src/main/kotlin/com/shubhasaimohapatra/plugins/ProjectRoutes.kt
@@ -11,47 +11,46 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.forEach
 import kotlinx.coroutines.withContext
 
+// Singleton for database operations
+object Database {
+    val client: MongoClient by lazy {
+        MongoClient.create(DatabaseUrl.url)
+    }
+
+    val database = client.getDatabase("portfolio")
+}
+
 suspend fun Application.getAllProjects(): List<ProjectDetails> {
-    val connectionString = DatabaseUrl.url
-    val mongoClient = MongoClient.create(connectionString)
-    val database = mongoClient.getDatabase("portfolio")
-    val collection = database.getCollection<ProjectDetails>("projects")
+    val collection = Database.database.getCollection<ProjectDetails>("projects")
     return try {
         withContext(Dispatchers.IO) {
             val list = mutableListOf<ProjectDetails>()
-            collection.find().collect { list.add(it) }
+            collection.find().collect {
+                list.add(it)
+            }
             list
         }
     } catch (e: Exception) {
         System.err.println("Error retrieving projects: $e")
         emptyList<ProjectDetails>()  // Return an empty list in case of an error
-    } finally {
-        mongoClient.close()
     }
 }
+
 suspend fun Application.addProject(projectDetails: ProjectDetails) {
-    val connectionString = DatabaseUrl.url
-    val mongoClient = MongoClient.create(connectionString)
-    val database = mongoClient.getDatabase("portfolio")
-    val collection = database.getCollection<ProjectDetails>("projects")
+    val collection = Database.database.getCollection<ProjectDetails>("projects")
 
     try {
         collection.insertOne(projectDetails)
     } catch (e: MongoException) {
         System.err.println("Unable to insert due to an error: $e")
         throw e  // Rethrow the exception to handle it in the route
-    } finally {
-        mongoClient.close()
     }
 }
+
 suspend fun Application.deleteProject(_projectId: String) {
-    val connectionString = DatabaseUrl.url
-    val mongoClient = MongoClient.create(connectionString)
-    val database = mongoClient.getDatabase("portfolio")
-    val collection = database.getCollection<ProjectDetails>("projects")
+    val collection = Database.database.getCollection<ProjectDetails>("projects")
     val query = Filters.eq(ProjectDetails::_id.name, _projectId)
-    println("Received From User Project Id $_projectId")
-    //println("Received From DB Project Id ${ProjectDetails::_id.name}")
+
     try {
         val result = collection.deleteOne(query)
         if (result.deletedCount.toInt() == 0) {
@@ -62,7 +61,5 @@ suspend fun Application.deleteProject(_projectId: String) {
     } catch (e: MongoException) {
         System.err.println("Unable to delete due to an error: $e")
         throw e  // Rethrow the exception to handle it in the route
-    } finally {
-        mongoClient.close()
     }
 }
